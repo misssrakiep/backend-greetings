d59d59f36189b0fb94062b544320c6748957db8c
misssrakiep <shameerarakiep@gmail.com>
2017-08-14 12:45:02 +0200

    fixed validation errors

--- greetedNames.js
+++ greetedNames.js
@@ -1,113 +1,92 @@
 module.exports = function(models) {
 
-    const userData = [];
-    const namesObj = {};
-    var count = 0;
+  const userData = [];
+  const namesObj = {};
+  var count = 0;
 
-    const index = function(req, res, next) {
-      var name = req.body.userName;
-      var language = req.body.language;
-      var newName = {
-        name: name,
-        count: 1
-      };
-
-      res.render('greet', {});
-
+  const index = function(req, res, next) {
+    var name = req.body.userName;
+    var language = req.body.language;
+    var newName = {
+      name: name,
+      count: 1
     };
 
-    const addNameScreen = function(req, res, next) {
-      models.Name.find({}, function(err, name) {
-        if (err) {
-          console.log(err);
-        } else {
-          res.render('greeted', {
-            name: name
-          })
-        }
-      });
+    res.render('greet', {});
+
+  };
+
+  const addNameScreen = function(req, res, next) {
+    models.Name.find({}, function(err, name) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('greeted', {
+          name: name,
+          count: count
+        })
+      }
+    });
+  };
+
+  const addName = function(req, res, next) {
+    var name = req.body.userName;
+    var language = req.body.language;
+    var newName = {
+      name: name,
+      count: 1
     };
 
-    const addName = function(req, res, next) {
-      var name = req.body.userName;
-      var language = req.body.language;
-      var newName = {
-        name: name,
-        count: 1
-      };
 
+    models.Name.findOne({
+      name: req.body.userName
+    }, function(err, naam) {
+      console.log('Found One');
+      console.log(naam);
+      if (err) {
+        return done(err);
+      }
 
-      models.Name.findOne({
-        name: req.body.userName
-      }, function(err, naam) {
-        console.log('Find One');
-        console.log(naam);
-        if (err) {
-          return done(err);
-        }
-
-        if (naam === null) {
-          console.log("Searched for this name");
-          models.Name.create({
-            name: req.body.userName,
-            count: 1
-          }, function(err, result) {
-            console.log('Created this name');
-            console.log(arguments);
-            if (err) {
-              return done(err);
-            }
-            res.render('greet', {
-              name: name,
-              language: language
-            });
+      if (naam === null) {
+        console.log("Searched for this name");
+        models.Name.create({
+          name: req.body.userName,
+          count: 1
+        }, function(err, result) {
+          console.log('Created this name');
+          console.log(arguments);
+          if (err) {
+            return done(err);
+          }
+          res.render('greet', {
+            name: name,
+            language: language
           });
-        }
-        if (naam !== null) {
-          // models.Name.find({
-          //   name: req.body.userData
-          // }, function(err, name) {
-          //   console.log('Second find one');
-          //   if (err) {
-          //     return done(err);
-          //   }
+        });
+      }
+      if (naam !== null) {
+        console.log('name', naam);
+        console.log(naam.count);
+        naam.count = naam.count + 1;
+        console.log(naam.count);
 
-            console.log('name', naam);
-            console.log(naam.count);
-            naam.count = naam.count + 1;
-            console.log(naam.count);
+        naam.save(function(err) {
+          if (err) {
+            console.log(err);
+          }
+          console.log('Reached the render');
+          res.render('greet', {
+            name: name,
+            language: language
+          });
+        });
+      };
+    });
+  };
 
-            naam.save(function(err){
-              if(err){
-                console.log(err);
-              }
-              console.log('Reached the render');
-              res.render('greet', {
-                namesObj: name,
-                count: count
-              });
-            });
-
-            // res.render('greeted',{name: name, count: count})
-
-        };
-
-
-      });
-    };
-
-
-
-    // if(name){
-    //   console.log(name.count)
-    //   name.count = name.count +1;
-    //   console.log(name.count);
-    // }
-
-
-    return {
-      index,
-      addNameScreen,
-      addName
-    };
+  return {
+    index,
+    addNameScreen,
+    addName
+  };
 }
--- models.js
+++ models.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = function(mongoUrl){
 	mongoose.Promise = global.Promise;
 	console.log(mongoUrl);
-	mongoose.connect(mongoUrl, function(err) {
+	mongoose.connect(mongoUrl, {useMongoClient: true}, function(err) {
 		if (err) {
 			console.log(err);
 		} else {
